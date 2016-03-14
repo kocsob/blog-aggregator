7a046407ee259245f4e6c52fdd13b1df1be90fb6
Balazs Kocso <kocsob@users.noreply.github.com>
2016-03-14 15:15:17 +0100

    Add logging and check user input

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 import argparse
 import json
 import logging
+import os
 
 from blog_aggregator import BlogAggregator
-
 
 logger = logging.getLogger()
 
@@ -14,17 +14,17 @@ def get_arguments():
         '-i',
         '--input',
         action='store',
-        default='/data/input',
-        dest='input',
-        help="input file or directory"
+        default='input.html',
+        dest='input_file',
+        help="input file"
     )
     parser.add_argument(
         '-o',
         '--output',
         action='store',
-        default='/data/output',
-        dest='output',
-        help="output file or directory"
+        default='output.json',
+        dest='output_file',
+        help="output file"
     )
     parser.add_argument(
         '-log',
@@ -45,7 +45,7 @@ def configure_logging(log_level="INFO"):
     logger.setLevel(numeric_log_level)
     ch = logging.StreamHandler()
     ch.setLevel(numeric_log_level)
-    formatter= logging.Formatter(
+    formatter = logging.Formatter(
         fmt='[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s',
         datefmt='%y%m%d %H:%M:%S'
     )
@@ -53,15 +53,34 @@ def configure_logging(log_level="INFO"):
     logger.addHandler(ch)
 
 
+def check_user_input(input_file, output_file):
+    if os.path.exists(input_file) is False:
+        raise ValueError("Input file '%s' does not exists!" % input_file)
+
+    if os.path.exists(os.path.dirname(output_file)) is False:
+        logger.info("Output directory '%s' does not exists, creating folder structure!" %
+                       os.path.dirname(output_file))
+        os.makedirs(os.path.dirname(output_file))
+
+
 def main():
     args = get_arguments()
     configure_logging(args.log_level)
-    logger.info('info')
 
-    blog_aggregator = BlogAggregator('index.html')
+    input_file = os.path.abspath(args.input_file)
+    output_file = os.path.abspath(args.output_file)
+    check_user_input(input_file, output_file)
+
+    logger.info("Process started with input file '%s'" % input_file)
+    blog_aggregator = BlogAggregator(input_file)
     feeds = blog_aggregator.aggregate()
-    with open('index.json', 'w') as fp:
+    with open(output_file, 'w') as fp:
         json.dump(feeds, fp, indent=4, sort_keys=True, encoding='utf-8')
+    logger.info("Process finished, the results are in output file '%s'" % output_file)
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.error(e)
--- blog_aggregator.py
+++ blog_aggregator.py
@@ -1,42 +1,54 @@
 import logging
 import socket
 
-from bs4 import BeautifulSoup
 import feedparser
-
+from bs4 import BeautifulSoup
 
 logger = logging.getLogger(__name__)
-socket.setdefaulttimeout(2)
+socket.setdefaulttimeout(5)
 
 
 class BlogAggregator(object):
-
     ATOM = 'atom'
     RSS = 'rss'
 
-    def __init__(self, file='index.html'):
+    def __init__(self, file='input.html'):
+        logger.debug("Open '%s' file to read" % file)
+
         with open(file, 'r') as fp:
             self.soup = BeautifulSoup(fp, 'html.parser')
 
     def get_links(self):
+        logger.debug("Get links from opended file")
+
         links = []
         for tag in self.soup.find_all(['a', 'link']):
             link = tag.get('href', None)
             if link is not None:
                 links.append(link)
+
+        logger.debug("%d link(s) found" % len(links))
+
         return links
 
     def get_feed_type(self, link):
+        logger.debug("Get feed type for '%s'" % link)
+
         feed = feedparser.parse(link)
         if feed.get('version', None) in ['atom', 'atom01', 'atom02', 'atom03', 'atom10']:
+            logger.debug("'%s' is an ATOM feed" % link)
             return self.ATOM
 
         if feed.get('version', None) in ['rss', 'rss090', 'rss091n', 'rss091u', 'rss092', 'rss093', 'rss094', 'rss10', 'rss20']:
+            logger.debug("'%s' is an RSS feed" % link)
             return self.RSS
 
+        logger.debug("'%s' is not a feed" % link)
         return None
 
     def aggregate(self):
+        logger.debug("Aggregate feeds from opened file")
+
         feeds = dict()
         feeds[self.ATOM] = []
         feeds[self.RSS] = []
